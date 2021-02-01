fa22da1e8635fe478394f9a666b562686c4c3219
Nilkumar Patel <=>
2021-02-01 18:59:12 -0500

    Group 8 Lab 3 Project 2 Step 3 (Setting caption and thumbnail)

--- Project 2/js/chapter09-project02.js
+++ Project 2/js/chapter09-project02.js
@@ -1,4 +1,20 @@
 /* Step-2 */
-window.addEventListner('load',function() {
+window.addEventListener('load',function() {
 	// body...
+	let images = document.querySelectorAll("#thumbnails img");
+	for (let i=0; i<images.length; i++) {
+		images[i].addEventListener('click', function() {
+			changeThumbnail(images[i]);
+		});
+	}
 });
+
+function changeThumbnail(img) {
+	// setting variables for thumbnail and captions
+	let thumbnail = document.querySelector('figure img');
+	let caption = document.querySelector('figure figcaption');
+	let imgPathArr = img.src.split("/");
+
+	thumbnail.src = "images/medium/" + imgPathArr[imgPathArr.length - 1];
+	caption.innerHTML = img.title;
+}
