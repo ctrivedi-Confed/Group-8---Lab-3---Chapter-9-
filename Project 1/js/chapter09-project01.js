5e281a6792bba481289b057e3e67869f53454bfb
Chintan Trivedi <chintantrivedi247@gmail.com>
2021-02-01 10:37:20 -0500

    added old project code

---
+++ Project 1/js/chapter09-project01.js
@@ -0,0 +1,25 @@
+
+/* add code here  */
+window.onload = function() {
+    let inputs = document.getElementsByClassName('hilightable');
+
+    for (let i=0; i<inputs.length; i++) {
+        inputs[i].addEventListener('focus', () => {
+            inputs[i].classList.toggle('highlight')
+        });
+        inputs[i].addEventListener('blur', () => {
+            inputs[i].classList.toggle('highlight')
+        });
+    }
+
+    document.getElementById("submit-btn").addEventListener('click', (e) => {
+        console.log("ljlj");
+        let validates = document.getElementsByClassName('required');
+        for(let i=0; i<validates.length;i++){
+            if(validates[i].value == '' || validates[i].value == null){
+                validates[i].classList.add('error');
+            }
+        }
+        e.preventDefault();
+    });
+};
