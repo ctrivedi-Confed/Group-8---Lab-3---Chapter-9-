b250958d864c34e1341bfb037c89564030eccae3
Chintan Trivedi <chintantrivedi247@gmail.com>
2021-02-01 19:53:31 -0500

    Group 8 Lab 3 Project 2 Step 4 (mouseover and mouseout)

--- Project 2/js/chapter09-project02.js
+++ Project 2/js/chapter09-project02.js
@@ -1,20 +1,40 @@
 /* Step-2 */
+
 window.addEventListener('load',function() {
 	// body...
 	let images = document.querySelectorAll("#thumbnails img");
+	let featured = document.querySelector('#featured img');
+	let caption = document.querySelector('#featured figcaption');
+
 	for (let i=0; i<images.length; i++) {
 		images[i].addEventListener('click', function() {
 			changeThumbnail(images[i]);
 		});
 	}
+
+	featured.addEventListener('mouseover', function() {
+		for (let i=0; i<images.length; i++) {
+			if (getRelativePath(images[i].src) == getRelativePath(featured.src)) {
+				caption.innerHTML = images[i].title;
+				break;
+			}
+		}
+		caption.style.opacity = 0.8;
+	});
+
+	featured.addEventListener('mouseout', function() {
+		caption.style.opacity = 0;
+	});
+
+	function changeThumbnail(img) {
+		// setting variables for thumbnail and captions
+		let imgPathArr = img.src.split("/");
+	
+		featured.src = "images/medium/" + imgPathArr[imgPathArr.length - 1];
+		caption.innerHTML = img.title;
+	}
 });
 
-function changeThumbnail(img) {
-	// setting variables for thumbnail and captions
-	let thumbnail = document.querySelector('figure img');
-	let caption = document.querySelector('figure figcaption');
-	let imgPathArr = img.src.split("/");
-
-	thumbnail.src = "images/medium/" + imgPathArr[imgPathArr.length - 1];
-	caption.innerHTML = img.title;
+function getRelativePath(path) {
+	return path.split('/')[path.split('/').length - 1]
 }
